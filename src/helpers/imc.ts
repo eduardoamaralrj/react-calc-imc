a9fff2c4018a7274855278281a5c762692d4a072
Eduardo Amaral <76413887+EduardoAmaralRJ@users.noreply.github.com>
2022-10-31 13:53:49 -0300

    Update imc.ts

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -7,11 +7,13 @@ export type Level = {
 }
 
 export const levels: Level [] = [
+  export const levels: Level [] = [
   { title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.4] },
   { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.5, 24.9] },
   { title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 29.9] },
-  { title: 'Obesidade', color:'#C3423F', icon: 'down', imc: [30, 39,9] },
-  { title: 'Obesidade Grave', color:'#C3423F', icon: 'down', imc: [40, 99] }
+  { title: 'Obesidade Grau I', color:'#C3423F', icon: 'down', imc: [30, 34,9] },
+  { title: 'Obesidade Grau II', color:'#C3423F', icon: 'down', imc: [35, 39,9] },
+  { title: 'Obesidade Grau III', color:'#C3423F', icon: 'down', imc: [40, 99] }
 ];
 
 export const calculateImc = (height: number, weight: number) => {
